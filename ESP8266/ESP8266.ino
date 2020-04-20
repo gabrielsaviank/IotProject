24e09864fb08efaa56776a61f047f87d2fd09c38
Gabriel Savian <gabrielsvnzrd@gmail.com>
2020-04-20 14:43:35 -0300

    Updated ide

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,57 +1,74 @@
-/*
-This example shows how to connect to Cayenne using an ESP8266 and send/receive sample data.
-
-The CayenneMQTT Library is required to run this sketch. If you have not already done so you can install it from the Arduino IDE Library Manager.
-
-Steps:
-1. If you have not already installed the ESP8266 Board Package install it using the instructions here: https://github.com/esp8266/Arduino#installing-with-boards-manager.
-2. Select your ESP8266 board from the Tools menu.
-3. Set the Cayenne authentication info to match the authentication info from the Dashboard.
-4. Set the network name and password.
-5. Compile and upload the sketch.
-6. A temporary widget will be automatically generated in the Cayenne Dashboard. To make the widget permanent click the plus sign on the widget.
-*/
-
 //#define CAYENNE_DEBUG
 #define CAYENNE_PRINT Serial
 #include <CayenneMQTTESP8266.h>
-#include <SimpleTimer.h>
-#include <SimpleDHT.h>
+
+#include <SimpleTimer.h> // Download from https://github.com/jfturcot/SimpleTimer
+#include <SimpleDHT.h> // Download from https://github.com/adafruit/DHT-sensor-library
+
 // WiFi network info.
 char ssid[] = "Luftwaffe";
-char wifiPassword[] = "7758773S";
+char wifiPassword[] = ""7758773S"";
 
 // Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "20e35100-831d-11ea-883c-638d8ce4c23d";
-char password[] = "4d0d22ac9757db688c9fb8ab5562565972bc79ba";
-char clientID[] = "4175e4f0-8327-11ea-b767-3f1a8f1211ba";
+char username[] = "<20e35100-831d-11ea-883c-638d8ce4c23d>";
+char password[] = "<4d0d22ac9757db688c9fb8ab5562565972bc79ba>";
+char clientID[] = "<4175e4f0-8327-11ea-b767-3f1a8f1211ba>";
 
+// DHT11 Pin
+int pinDHT11 = 2;
+SimpleDHT11 dht11;
+
+// Sound Pin
+int soundPin = 3;
+
+// Relay Pin
+int relayPin = 1;
+
+// Timer
+SimpleTimer timer;
 
 void setup() {
-	Serial.begin(9600);
-	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+ Serial.begin(9600);
+ Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+ pinMode(relayPin, OUTPUT); // Relay
+ digitalWrite(relayPin, HIGH);
+ pinMode(soundPin, INPUT); // Sound
+ timer.setInterval(200L, transmitData); // Method to execute every 200ms
 }
 
 void loop() {
-	Cayenne.loop();
+ Cayenne.loop();
+ timer.run();
 }
 
-// Default function for sending sensor data at intervals to Cayenne.
-// You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
-CAYENNE_OUT_DEFAULT()
-{
-	// Write data to Cayenne here. This example just sends the current uptime in milliseconds on virtual channel 0.
-	Cayenne.virtualWrite(0, millis());
-	// Some examples of other functions you can use to send data.
-	//Cayenne.celsiusWrite(1, 22.0);
-	//Cayenne.luxWrite(2, 700);
-	//Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
+CAYENNE_IN(relayPin) {
+ if (getValue.asInt() == 1) { // NOTE: Channel = Virtual Pin
+ digitalWrite(relayPin, LOW);
+ }
+ else {
+ digitalWrite(relayPin, HIGH);
+ }
 }
 
-// Default function for processing actuator commands from the Cayenne Dashboard.
-// You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
-CAYENNE_IN_DEFAULT()
+void transmitData()
 {
-	CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
-	//Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
+ byte temperature = 0;
+ byte humidity = 0;
+ int err = SimpleDHTErrSuccess;
+
+if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+ Cayenne.virtualWrite(V4, 0);
+ Cayenne.virtualWrite(V2, 0);
+ }
+ else {
+ Cayenne.virtualWrite(V4, (int)temperature);
+ Cayenne.virtualWrite(V2, (int)humidity);
+ }
+
+if (digitalRead(soundPin) == HIGH) {
+ Cayenne.virtualWrite(V3, HIGH);
+ }
+ else {
+ Cayenne.virtualWrite(V3, LOW);
+ }
 }
